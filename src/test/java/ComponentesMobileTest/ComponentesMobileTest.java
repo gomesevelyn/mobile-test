fb6750f731260564973c060e9b3d08b26d0a4536
Evelyn Gomes <evelyngomes84@gmail.com>
2021-09-19 21:56:32 -0300

    Ajustes no codigo de teste

--- src/test/java/ComponentesMobileTest/ComponentesMobileTest.java
+++ src/test/java/ComponentesMobileTest/ComponentesMobileTest.java
@@ -2,6 +2,7 @@ package ComponentesMobileTest;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.List;
 
 import org.junit.After;
 import org.junit.Assert;
@@ -11,20 +12,26 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized.Parameters;
 import org.junit.runners.Parameterized;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
+
+import io.appium.java_client.MobileBy;
+//import edu.emory.mathcs.backport.java.util.concurrent.TimeUnit;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
 
+import java.util.concurrent.TimeUnit;
+
 public class ComponentesMobileTest {
 
-	private AndroidDriver<MobileElement> driver;
+	 private AndroidDriver<MobileElement> driver;
 
 	@Before
 	public void inicializarAppium() throws MalformedURLException {
 
 		DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
-
+		/*
 		// Set your access credentials
 		desiredCapabilities.setCapability("browserstack.user", "evelyngomes_OscVB5");
 		desiredCapabilities.setCapability("browserstack.key", "qJAcyDz5GqdTqWqELAaq");
@@ -37,28 +44,32 @@ public class ComponentesMobileTest {
 		desiredCapabilities.setCapability("project", "Test Jenkins with BrowserStack");
 		desiredCapabilities.setCapability("build", "Java Android");
 		desiredCapabilities.setCapability("name", "first_test");
+		*/
 
-		/*
-		 * desiredCapabilities.setCapability("deviceName", "emulator-5554");
-		 * desiredCapabilities.setCapability("udid", "emulator-5554");
-		 * desiredCapabilities.setCapability("platformName", "Android");
-		 * desiredCapabilities.setCapability("platformVersion", "9.0");
-		 * desiredCapabilities.setCapability("automationName", "UiAutomator2");
-		 */
+		
+		  desiredCapabilities.setCapability("deviceName", "Emulator");
+		  desiredCapabilities.setCapability("udid", "K3AXB6036169X7L");
+		  desiredCapabilities.setCapability("platformName", "Android");
+		  //desiredCapabilities.setCapability("platformVersion", "9.0");
+		  desiredCapabilities.setCapability("automationName", "UiAutomator2");
+		  
+		  driver = new AndroidDriver<MobileElement> (new URL("http://localhost:4723/wd/hub"),desiredCapabilities); 
+		  
 
 		// instrucao para que o appium faca a instalacao do APK
 		desiredCapabilities.setCapability(MobileCapabilityType.APP,
 				"/Users/Evely/eclipse-workspace/br.com.evelyn.Components/src/main/resources/CTAppium_1_2.apk");
 
 		// Initialise the remote Webdriver using BrowserStack remote URL
-		URL remoteUrl = new URL("http://hub.browserstack.com/wd/hub");
+		//URL remoteUrl = new URL("http://hub.browserstack.com/wd/hub");
+		URL remoteUrl = new URL("http://localhost:4723/wd/hub");
 		driver = new AndroidDriver<MobileElement>(remoteUrl, desiredCapabilities);
-
-	}
-
-	@Test
-	public void acessaFormulario() {
-		driver.findElement(By.xpath("//*[text='Formulário']")).click();
+		
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		
+		//Selecionar Formulario
+		driver.findElement(By.xpath("//*[@text='Formulário']")).click();
+		
 	}
 
 	@Test
@@ -67,18 +78,22 @@ public class ComponentesMobileTest {
 		driver.findElement(By.className("android.widget.EditText")).sendKeys("Teste Inclusão");
 		driver.findElement(By.className("android.widget.CheckBox")).click();
 		driver.findElement(By.className("android.widget.Switch")).click();
-		driver.findElement(By.className("android.widget.Snipper")).click(); // comboBox
-		driver.findElement(By.xpath("android.widget.ChekedTextViewer[@text='Nintendo Switch']")).click();
-		// Assert.assertEquals("Nintendo Switch", text);
-		driver.findElement(By.xpath("//*[text='SALVAR']")).click();
+		//driver.findElement(By.className("android.widget.Snipper")).click(); 
+		/* comboBox
+		driver.findElement(MobileBy.AccessibilityId("console")).click();
+		driver.findElement(By.xpath("//android.widget.CheckedTextViewer[@text='PS4']")).click();*/
+		
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		driver.findElement(By.xpath("//*[@text='SALVAR']")).click();
 
 		// Verificações 
 		MobileElement nome = driver.findElement(By.xpath("//android.widget.TextView[@text='Nome: Teste Inclusão']"));
 		Assert.assertEquals("Nome: Teste Inclusão", nome.getText());
 		
-		//outra forma de validacao de elementos 
+		/*outra forma de validacao de elementos 
 		MobileElement combo = driver.findElement(By.xpath("//android.widget.TextView[starts-with(@text, 'Console:')]")); 
-		Assert.assertEquals("Console: switch", combo.getText());
+		Assert.assertEquals("Console: switch", combo.getText());*/
 		
 		//outra forma de validacao de elementos 
 		MobileElement switc = driver.findElement(By.xpath("//android.widget.TextView[starts-with(@text, 'Switch:')]")); 
